d5a40542b69ff4e2d9feaa7c5f9dd4f3d0cfadb0
Tianhao Zhou <tianhaoz@umich.edu>
2021-07-11 00:57:53 +0000

    fix: add logging sections

--- scripts/tasks/main.sh
+++ scripts/tasks/main.sh
@@ -2,6 +2,14 @@
 
 SUBTASK_SCRIPT_DIR="$PROJECT_ROOT/scripts/tasks/subtasks"
 
+echo "::group::Install Basic Dependencies"
 source "$SUBTASK_SCRIPT_DIR/install_basic_deps.sh"
+echo "::endgroup::"
+
+echo "::group::Install xfce4"
 ! "$CI" && source "$SUBTASK_SCRIPT_DIR/install_xfce.sh"
+echo "::endgroup::
+
+echo "::group::Install CRD"
 source "$SUBTASK_SCRIPT_DIR/install_crd.sh"
+echo "::endgroup::
