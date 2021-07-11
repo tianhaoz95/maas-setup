5c1ac8db1e5db6092a39bf010de5900ac543dc9b
Tianhao Zhou <tianhaoz@umich.edu>
2021-07-11 00:52:03 +0000

    fix: skip xfce for now

--- scripts/tasks/main.sh
+++ scripts/tasks/main.sh
@@ -3,5 +3,5 @@
 SUBTASK_SCRIPT_DIR="$PROJECT_ROOT/scripts/tasks/subtasks"
 
 source "$SUBTASK_SCRIPT_DIR/install_basic_deps.sh"
-source "$SUBTASK_SCRIPT_DIR/install_xfce.sh"
+! "$CI" && source "$SUBTASK_SCRIPT_DIR/install_xfce.sh"
 source "$SUBTASK_SCRIPT_DIR/install_crd.sh"
