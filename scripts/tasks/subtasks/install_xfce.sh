15de99e28b1174e30510367368d4b357280f219a
Tianhao Zhou <tianhaoz@umich.edu>
2021-07-11 01:08:35 +0000

    fix: remove xfce4 in CI

--- scripts/tasks/subtasks/install_xfce.sh
+++ scripts/tasks/subtasks/install_xfce.sh
@@ -5,6 +5,9 @@ figlet "Install Desktop"
 sudo apt-get update -y
 
 DEBIAN_FRONTEND=noninteractive \
-    sudo apt-get install -y desktop-base xscreensaver xfce4
+    sudo apt-get install -y desktop-base xscreensaver
+
+! "$CI" && DEBIAN_FRONTEND=noninteractive \
+    sudo apt-get install -y xfce4
 
 ! "$CI" && sudo echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session
