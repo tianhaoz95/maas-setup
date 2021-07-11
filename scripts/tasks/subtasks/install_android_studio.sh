dc1450aad694f687b918c57fb9b1032aca0261d1
Tianhao Zhou <tianhaoz@umich.edu>
2021-07-11 17:35:54 +0000

    fix: wrong slash

--- scripts/tasks/subtasks/install_android_studio.sh
+++ scripts/tasks/subtasks/install_android_studio.sh
@@ -7,7 +7,7 @@ function download_and_extract_android_studio {
     LINUX_ANDROID_STUDIO_DOWNLOAD_URL="https://redirector.gvt1.com/edgedl/android/studio/ide-zips/4.2.2.0/android-studio-ide-202.7486908-linux.tar.gz"
     ANDROID_STUDIO_INSTALL_DIR="$HOME/tools"
     ANDROID_PLATFORM_TOOLS_INSTALL_DIR="$HOME/Android/Sdk/platform-tools"
-    add_path_to_bashrc "$ANDROID_STUDIO_INSTALL_DIR\bin"
+    add_path_to_bashrc "$ANDROID_STUDIO_INSTALL_DIR/bin"
     add_path_to_bashrc "$ANDROID_PLATFORM_TOOLS_INSTALL_DIR"
     mkdir -p $ANDROID_STUDIO_INSTALL_DIR
     download_and_extract \
