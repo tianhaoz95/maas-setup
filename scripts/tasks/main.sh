322b4989f6cf469c7df68e3df8a7503a85d759e1
Tianhao Zhou <tianhaoz@umich.edu>
2021-07-11 06:44:43 +0000

    feat: add nvidia drvier installation

--- scripts/tasks/main.sh
+++ scripts/tasks/main.sh
@@ -6,6 +6,11 @@ echo "::group::Install Basic Dependencies"
 source "$SUBTASK_SCRIPT_DIR/install_basic_deps.sh"
 echo "::endgroup::"
 
+echo "::group::Install Nvidia driver"
+"$CI" && echo "Install Nvidia driver in CI container will not work. Skip."
+! "$CI" && source "$SUBTASK_SCRIPT_DIR/install_nvidia_driver.sh"
+echo "::endgroup::"
+
 echo "::group::Install xfce4"
 "$CI" && echo "Install desktop in CI container will not work. Skip."
 ! "$CI" && source "$SUBTASK_SCRIPT_DIR/install_xfce.sh"
