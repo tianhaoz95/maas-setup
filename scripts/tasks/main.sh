59d744a68aa4ffcece2faa790266717a4b002765
Tianhao Zhou <tianhaoz@umich.edu>
2021-07-11 00:26:14 +0000

    fix: use source

--- scripts/tasks/main.sh
+++ scripts/tasks/main.sh
@@ -2,5 +2,6 @@
 
 SUBTASK_SCRIPT_DIR="$PROJECT_ROOT/scripts/tasks/subtasks"
 
-bash "$SUBTASK_SCRIPT_DIR/install_basic_deps.sh"
-bash "$SUBTASK_SCRIPT_DIR/install_crd.sh"
+source "$SUBTASK_SCRIPT_DIR/install_basic_deps.sh"
+source "$SUBTASK_SCRIPT_DIR/install_xfce.sh"
+source "$SUBTASK_SCRIPT_DIR/install_crd.sh"
