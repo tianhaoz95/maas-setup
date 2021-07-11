60061b1e80bc730123997ce0104120605f718e8b
Tianhao Zhou <tianhaoz@umich.edu>
2021-07-11 08:15:09 +0000

    fix: set default crd

--- scripts/tasks/subtasks/install_xfce.sh
+++ scripts/tasks/subtasks/install_xfce.sh
@@ -6,7 +6,7 @@ function install_xfce {
     sudo apt-get update -y
     DEBIAN_FRONTEND=noninteractive \
         sudo apt-get install -y desktop-base xscreensaver xfce4
-    sudo echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session
+    sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 }
 
 install_xfce
