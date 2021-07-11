67c7096c9ce518f5f0cc0fda5959db9f1135752a
Tianhao Zhou <tianhaoz@umich.edu>
2021-07-11 01:04:40 +0000

    fix: disable config for CI

--- scripts/tasks/subtasks/install_xfce.sh
+++ scripts/tasks/subtasks/install_xfce.sh
@@ -7,4 +7,4 @@ sudo apt-get update -y
 DEBIAN_FRONTEND=noninteractive \
     sudo apt-get install -y desktop-base xscreensaver xfce4
 
-sudo echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session
+! "$CI" && sudo echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session
